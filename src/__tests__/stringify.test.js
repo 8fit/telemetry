ad53a874145e6dd8a06c78ddd3897a1b93402aa2
Pedro Solá <p3dro.sola@gmail.com>
2018-06-22 11:52:31 +0200

    Fix escaping

--- src/__tests__/stringify.test.js
+++ src/__tests__/stringify.test.js
@@ -1,70 +1,85 @@
-import { 
-  _stringifyPoint,
-  stringifyPoints,
-} from '../stringify';
+import { _stringifyPoint, stringifyPoints } from "../stringify";
 
-describe('_stringifyPoint', () => {
-  it('Returns a string version of a point (measurement, value, tags, timestamp)', () => {
-    const measurement = 'payment';
+describe("_stringifyPoint", () => {
+  it("Returns a string version of a point (measurement, value, tags, timestamp)", () => {
+    const measurement = "payment";
     const values = {
       billed: 33,
-      licenses: '3i',
-    }
+      licenses: "3i"
+    };
 
     const tags = {
-      device: 'mobile',
-      product: 'Notepad',
-      method: 'credit',
+      device: "mobile",
+      product: "Notepad",
+      method: "credit"
     };
-    const timestamp = '1434067467100293230';
+    const timestamp = "1434067467100293230";
 
     const result = _stringifyPoint({
       measurement,
       values,
       tags,
-      timestamp,
+      timestamp
     });
 
     expect(result).toBe(
       'payment,device=mobile,product=Notepad,method=credit billed=33,licenses="3i" 1434067467100293230'
     );
   });
+  it("escapes tags", () => {
+    const result = _stringifyPoint({
+      measurement: "payment",
+      values: { billed: 33 },
+      tags: { "tag name": "tag,value" },
+      timestamp: 99
+    });
+    expect(result).toBe(`payment,tag\\ name=tag\\,value billed=33 99`);
+  });
+  it("escapes measurements", () => {
+    const result = _stringifyPoint({
+      measurement: "payment",
+      values: { billed: '33"44' },
+      tags: {},
+      timestamp: 99
+    });
+    expect(result).toBe(`payment, billed="33\\"44" 99`);
+  });
 });
 
-describe('stringifyPoints', () => {
-  it('returns a influxdb compatible string with 1 or more points', () => {
+describe("stringifyPoints", () => {
+  it("returns a influxdb compatible string with 1 or more points", () => {
     const point1 = {
-      measurement: 'test_measurement1',
+      measurement: "test_measurement1",
       values: {
         value1: 42,
         value2: true,
-        value3: 'string',
+        value3: "string"
       },
       tags: {
-        host: 'test_host',
-        region: 'us-west',
+        host: "test_host",
+        region: "us-west"
       },
-      timestamp: 999,
-    }
+      timestamp: 999
+    };
 
     const point2 = {
-      measurement: 'test_measurement2',
+      measurement: "test_measurement2",
       values: {
-        value1: 'test',
+        value1: "test"
       },
       tags: {
-        host: 'test_host',
-        region: 'us-west',
+        host: "test_host",
+        region: "us-west"
       },
-      timestamp: 1500,
-    }
+      timestamp: 1500
+    };
 
     const result = stringifyPoints([point1, point2]);
-    
+
     expect(result).toBe(
       'test_measurement1,host=test_host,region=us-west value1=42,value2=true,value3="string" 999' +
-      '\n' +
-      'test_measurement2,host=test_host,region=us-west value1="test" 1500'
+        "\n" +
+        'test_measurement2,host=test_host,region=us-west value1="test" 1500'
     );
   });
 });
--- src/stringify.js
+++ src/stringify.js
@@ -1,24 +1,34 @@
+// https://docs.influxdata.com/influxdb/v1.5/write_protocols/line_protocol_tutorial/#special-characters-and-keywords
+
 export function stringifyPoints(points) {
-  return points
-    .map(_stringifyPoint)
-    .join('\n');
+  return points.map(_stringifyPoint).join("\n");
 }
 
 export function _stringifyPoint({ measurement, values, tags, timestamp }) {
-  const tagsString = Object.keys(tags)
-    .map(key => `${key}=${tags[key]}`)
-    .join();
-  
-  const valuesString = Object.keys(values)
-    .map(key => `${key}=${_formatValue(values[key])}`)
-    .join();
+  const tagsString = Object.entries(tags)
+    .map(([key, value]) => {
+      return `${escapeTag(key)}=${escapeTag(value)}`;
+    })
+    .join(",");
+
+  const valuesString = Object.entries(values)
+    .map(([key, value]) => {
+      return `${escapeTag(key)}=${_formatValue(value)}`;
+    })
+    .join(",");
 
   return `${measurement},${tagsString} ${valuesString} ${timestamp}`;
 }
 
-const _formatValue = (value) => {
-  if (typeof value === 'string') { 
-    return `"${value}"`;
+const _formatValue = value => {
+  if (typeof value === "string") {
+    return `"${value.replace('"', '\\"')}"`;
   }
   return value;
 };
+
+const escapeTag = value =>
+  value
+    .replace(",", "\\,")
+    .replace(" ", "\\ ")
+    .replace("=", "\\=");
